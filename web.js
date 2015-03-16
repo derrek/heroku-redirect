bf19ed358a58c85cfd1ed658fe85ef64be6d268b
Derrek Long <derrek.long@gmail.com>
2015-03-16 14:05:21 -0700

    make redirect a 301 permanent instead of 302 temporary

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('*', function(request, response) {
-  response.redirect(process.env.NEW_BASE_URL + request.url)
+  response.redirect(301, process.env.NEW_BASE_URL + request.url)
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
